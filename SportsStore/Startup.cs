49dc6b65f21ce8db55d7b241924fcba5a9471cdb
mareveles <mareveles1@buffs.wtamu.edu>
2018-04-01 21:40:54 -0500

    First commit

---
+++ SportsStore/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup{
+        public void ConfigureServices(IServiceCollection services){
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+            });
+        }
+    }
+}
