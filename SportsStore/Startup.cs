099ab24bdcfe329b544ffb7ec4626e4ba4439d34
mareveles <mareveles1@buffs.wtamu.edu>
2018-04-01 22:44:45 -0500

    new files

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using SportsStore.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SportsStore.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SportsStore
 {
     public class Startup{
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services){
-            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlite("Data Source=SportsStore.db"));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
