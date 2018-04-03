a543a6609e1832916471269c21a7ee51c8ee75b2
mareveles <mareveles1@buffs.wtamu.edu>
2018-04-02 22:43:15 -0500

    added model

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -17,11 +19,15 @@ namespace MvcMovie
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlite("Data Source=MvcMovie.db"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
